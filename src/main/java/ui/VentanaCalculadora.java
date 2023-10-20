2113d83c83974efa9ef80c8d3b902394ed12dbc0
jorgeoj <jorge2002oj@gmail.com>
2023-10-20 13:35:40 +0200

    Mejora de codigo (añadir lambdas y alguna mejora)

--- src/main/java/ui/VentanaCalculadora.java
+++ src/main/java/ui/VentanaCalculadora.java
@@ -1,8 +1,6 @@
 package ui;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Objects;
 
 /**
@@ -43,203 +41,146 @@ public class VentanaCalculadora extends JFrame{
     public VentanaCalculadora() {
 
         //Action Listener boton AC
-        buttonAC.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText("");
-            }
+        buttonAC.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText("");
         });
         //Action listener boton 9
-        button9.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button9.getText());
-            }
+        button9.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button9.getText());
         });
         //Action listener boton 8
-        button8.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button8.getText());
-            }
+        button8.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button8.getText());
         });
         //Action Listener boton 7
-        button7.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button7.getText());
-            }
+        button7.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button7.getText());
         });
         //Action Listener boton 6
-        button6.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button6.getText());
-            }
+        button6.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button6.getText());
         });
         //Action Listener boton 5
-        button5.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button5.getText());
-            }
+        button5.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button5.getText());
         });
         //Action Listener boton 4
-        button4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button4.getText());
-            }
+        button4.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button4.getText());
         });
         //Action Listener boton 3
-        button3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button3.getText());
-            }
+        button3.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button3.getText());
         });
         //Action Listener boton 2
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button2.getText());
-            }
+        button2.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button2.getText());
         });
         //Action Listener boton 1
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button1.getText());
-            }
+        button1.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button1.getText());
         });
         //Action Listener boton 0
-        button0.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonBorrar.setEnabled(true);
-                txtSalida.setText(txtSalida.getText()+ button0.getText());
-            }
+        button0.addActionListener(e -> {
+            buttonBorrar.setEnabled(true);
+            txtSalida.setText(txtSalida.getText()+ button0.getText());
         });
         //Action listener boton . (coma)
-        buttonComa.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //Controlar que el numero no tenga ya una coma
-                if (!txtSalida.getText().contains(".")){
-                    txtSalida.setText(txtSalida.getText() + buttonComa.getText());
-                }
+        buttonComa.addActionListener(e -> {
+            //Controlar que el numero no tenga ya una coma
+            if (!txtSalida.getText().contains(".")){
+                txtSalida.setText(txtSalida.getText() + buttonComa.getText());
             }
         });
         //Action listener boton sumar
-        buttonSuma.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                op1 = Double.parseDouble(txtSalida.getText());
-                operacion = "+";
-                txtSalida.setText("");
-            }
+        buttonSuma.addActionListener(e -> {
+            op1 = Double.parseDouble(txtSalida.getText());
+            operacion = "+";
+            txtSalida.setText("");
         });
         //Action listener boton restar
-        buttonResta.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                op1 = Double.parseDouble(txtSalida.getText());
-                operacion = "-";
-                txtSalida.setText("");
-            }
+        buttonResta.addActionListener(e -> {
+            op1 = Double.parseDouble(txtSalida.getText());
+            operacion = "-";
+            txtSalida.setText("");
         });
         //Action listener boton multiplicar
-        buttonMultiplicar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                op1 = Double.parseDouble(txtSalida.getText());
-                operacion = "*";
-                txtSalida.setText("");
-            }
+        buttonMultiplicar.addActionListener(e -> {
+            op1 = Double.parseDouble(txtSalida.getText());
+            operacion = "*";
+            txtSalida.setText("");
         });
         //Action listener boton dividir
 
-        buttonDividir.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                op1 = Double.parseDouble(txtSalida.getText());
-                operacion = "/";
-                txtSalida.setText("");
-            }
+        buttonDividir.addActionListener(e -> {
+            op1 = Double.parseDouble(txtSalida.getText());
+            operacion = "/";
+            txtSalida.setText("");
         });
 
         //Action listener boton +-
-        buttonMasMenos.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!txtSalida.getText().isEmpty() && !Objects.equals(txtSalida.getText(), "0")){
-                    double num = Double.parseDouble(txtSalida.getText());
-                    num = num * -1;
-                    if (num == (int) num) {
-                        txtSalida.setText(String.valueOf((int) num));
-                    } else {
-                        txtSalida.setText(String.valueOf(num));
-                    }
+        buttonMasMenos.addActionListener(e -> {
+            if (!txtSalida.getText().isEmpty() && !Objects.equals(txtSalida.getText(), "0")){
+                double num = Double.parseDouble(txtSalida.getText());
+                num = num * -1;
+                if (num == (int) num) {
+                    txtSalida.setText(String.valueOf((int) num));
+                } else {
+                    txtSalida.setText(String.valueOf(num));
                 }
             }
         });
 
         //Action listener boton borrar
-        buttonBorrar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String retroceso = null;
-                //Controlar que borre solo si hay algo
-                if (txtSalida.getText().length()>0){
-                    StringBuilder sb = new StringBuilder(txtSalida.getText());
-                    //Borrar el ultimo elemento del texto
-                    sb.deleteCharAt(txtSalida.getText().length() - 1);
-                    retroceso = String.valueOf(sb);
-                    txtSalida.setText(retroceso);
-                }
+        buttonBorrar.addActionListener(e -> {
+            String retroceso;
+            //Controlar que borre solo si hay algo
+            if (!txtSalida.getText().isEmpty()){
+                StringBuilder sb = new StringBuilder(txtSalida.getText());
+                //Borrar el ultimo elemento del texto
+                sb.deleteCharAt(txtSalida.getText().length() - 1);
+                retroceso = String.valueOf(sb);
+                txtSalida.setText(retroceso);
             }
         });
         //Action listener boton igual
-        buttonIgual.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                op2 = Double.parseDouble(txtSalida.getText());
-                buttonBorrar.setEnabled(false);
+        buttonIgual.addActionListener(e -> {
+            op2 = Double.parseDouble(txtSalida.getText());
+            buttonBorrar.setEnabled(false);
 
 
-                switch (operacion){
-                    case "+":
-                        resultado = op1 + op2;
-                        break;
-                    case "-":
-                        resultado = op1 - op2;
-                        break;
-                    case "*":
-                        resultado = op1 * op2;
-                        break;
-                    case "/":
-                        resultado = op1 / op2;
-                        break;
-                }
+            switch (operacion){
+                case "+":
+                    resultado = op1 + op2;
+                    break;
+                case "-":
+                    resultado = op1 - op2;
+                    break;
+                case "*":
+                    resultado = op1 * op2;
+                    break;
+                case "/":
+                    resultado = op1 / op2;
+                    break;
+            }
 
+            txtSalida.setText(String.valueOf(resultado));
+
+            //Para que el resultado salga entero o double
+            if(resultado % 1 == 0){
+                txtSalida.setText(String.valueOf((int) resultado));
+            }else{
                 txtSalida.setText(String.valueOf(resultado));
-
-                //Para que el resultado salga entero o double
-                if(resultado % 1 == 0){
-                    txtSalida.setText(String.valueOf((int) resultado));
-                }else{
-                    txtSalida.setText(String.valueOf(resultado));
-                }
             }
         });
 
